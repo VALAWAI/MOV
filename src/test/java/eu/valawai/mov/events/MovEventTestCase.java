f26ce8972bb11d400cc772978f732da2ac104e2c
Bruno Rosell i Gui <rosell@iiia.csic.es>
2024-03-11 12:32:56 +0100

    Fiz test get query page

--- src/test/java/eu/valawai/mov/events/MovEventTestCase.java
+++ src/test/java/eu/valawai/mov/events/MovEventTestCase.java
@@ -13,16 +13,19 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.AfterEach;
 
 import eu.valawai.mov.MasterOfValawaiTestCase;
 import io.quarkus.logging.Log;
+import io.vertx.core.json.JsonObject;
 import jakarta.inject.Inject;
 
 /**
@@ -149,6 +152,218 @@ public class MovEventTestCase extends MasterOfValawaiTestCase {
 
 		assertEquals(1, errors.size());
 		return errors.get(0);
+
+	}
+
+	/**
+	 * A queue that can be used to manage the received messages.
+	 *
+	 * @param messages  that has been received.
+	 * @param errors    that has been received.
+	 * @param semaphore to release for every error or received message.
+	 */
+	public record TestMQQueue(List<JsonObject> messages, List<Throwable> errors, Semaphore semaphore) {
+
+		/**
+		 * Create a new queue.
+		 */
+		public TestMQQueue() {
+
+			this(Collections.synchronizedList(new ArrayList<>()), Collections.synchronizedList(new ArrayList<>()),
+					new Semaphore(0));
+		}
+
+		/**
+		 * Wait until received a message that satisfy a filter.
+		 *
+		 * @param filter   of the waiting message.
+		 * @param duration maximum time to wait.
+		 *
+		 * @return the received message.
+		 *
+		 * @throws AssertionError if no message received.
+		 */
+		public JsonObject waitReceiveMessage(Predicate<JsonObject> filter, Duration duration) {
+
+			synchronized (this.messages) {
+
+				final var max = this.messages.size();
+				for (var i = 0; i < max; i++) {
+
+					final var msg = this.messages.get(i);
+					if (filter.test(msg)) {
+
+						this.messages.remove(i);
+						return msg;
+					}
+				}
+
+				try {
+
+					this.semaphore.tryAcquire(duration.toMillis(), TimeUnit.MILLISECONDS);
+
+				} catch (final Throwable ignored) {
+				}
+			}
+
+			synchronized (this.messages) {
+
+				final var max = this.messages.size();
+				for (var i = 0; i < max; i++) {
+
+					final var msg = this.messages.get(i);
+					if (filter.test(msg)) {
+
+						this.messages.remove(i);
+						return msg;
+					}
+				}
+			}
+
+			fail("Not received the message in the specified time.");
+			return null;
+		}
+
+		/**
+		 * Wait at maximum 30 seconds to receive any message.
+		 *
+		 * @return the received message.
+		 *
+		 * @throws AssertionError if no message received.
+		 */
+		public JsonObject waitReceiveMessage() {
+
+			return this.waitReceiveMessage(any -> true, Duration.ofSeconds(30));
+		}
+
+		/**
+		 * Wait at maximum 30 seconds to receive any message with the specified payload.
+		 *
+		 * @param payloadType to receive
+		 *
+		 * @return the received payload.
+		 *
+		 * @throws AssertionError if no message received.
+		 */
+		public <T> T waitReceiveMessage(Class<T> payloadType) {
+
+			final var msg = this.waitReceiveMessage(payload -> {
+
+				try {
+
+					payload.mapTo(payloadType);
+					return true;
+
+				} catch (final Throwable isNotTheExpectedMessage) {
+
+					return false;
+				}
+
+			}, Duration.ofSeconds(30));
+
+			return msg.mapTo(payloadType);
+		}
+
+		/**
+		 * Wait until received a error that satisfy a filter.
+		 *
+		 * @param filter   of the waiting error.
+		 * @param duration maximum time to wait.
+		 *
+		 * @return the received error.
+		 *
+		 * @throws AssertionError if no error received.
+		 */
+		public Throwable waitReceiveError(Predicate<Throwable> filter, Duration duration) {
+
+			synchronized (this.errors) {
+
+				final var max = this.errors.size();
+				for (var i = 0; i < max; i++) {
+
+					final var error = this.errors.get(i);
+					if (filter.test(error)) {
+
+						this.errors.remove(i);
+						return error;
+					}
+				}
+
+				try {
+
+					this.semaphore.tryAcquire(duration.toMillis(), TimeUnit.MILLISECONDS);
+
+				} catch (final Throwable ignored) {
+				}
+			}
+
+			synchronized (this.errors) {
+
+				final var max = this.errors.size();
+				for (var i = 0; i < max; i++) {
+
+					final var error = this.errors.get(i);
+					if (filter.test(error)) {
+
+						this.errors.remove(i);
+						return error;
+					}
+				}
+			}
+
+			fail("Not received the error in the specified time.");
+			return null;
+		}
+
+		/**
+		 * Wait at maximum 30 seconds to receive any error.
+		 *
+		 * @return the received error.
+		 *
+		 * @throws AssertionError if no error received.
+		 */
+		public Throwable waitReceiveError() {
+
+			return this.waitReceiveError(any -> true, Duration.ofSeconds(30));
+		}
+	}
+
+	/**
+	 * Open a queue and wait at maximum 30 seconds to be opened.
+	 *
+	 * @param queueName name of the queue to open.
+	 *
+	 * @return the opened queue.
+	 */
+	public TestMQQueue waitOpenQueue(String queueName) {
+
+		return this.waitOpenQueue(queueName, Duration.ofSeconds(30));
+
+	}
+
+	/**
+	 * Open a queue and wait at maximum the duration to be opened.
+	 *
+	 * @param queueName name of the queue to open.
+	 * @param duration  maximum time to wait.
+	 *
+	 * @return the opened queue.
+	 */
+	public TestMQQueue waitOpenQueue(String queueName, Duration duration) {
+
+		final var queue = new TestMQQueue();
+		this.listener.open(queueName).subscribe().with(msg -> {
+
+			queue.messages.add(msg);
+			queue.semaphore.release();
+
+		}, error -> {
+
+			queue.errors.add(error);
+			queue.semaphore.release();
+		});
+		this.waitUntilQueueIsOpen(queueName, duration);
+		return queue;
 
 	}
 
--- src/test/java/eu/valawai/mov/events/components/QueryComponentsManagerTest.java
+++ src/test/java/eu/valawai/mov/events/components/QueryComponentsManagerTest.java
@@ -8,16 +8,26 @@
 
 package eu.valawai.mov.events.components;
 
+import static eu.valawai.mov.ValueGenerator.next;
 import static eu.valawai.mov.ValueGenerator.nextUUID;
+import static eu.valawai.mov.ValueGenerator.rnd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
+import com.mongodb.client.model.Filters;
+
+import eu.valawai.mov.api.v1.components.ComponentType;
 import eu.valawai.mov.api.v1.logs.LogLevel;
 import eu.valawai.mov.events.MovEventTestCase;
+import eu.valawai.mov.persistence.components.ComponentEntities;
+import eu.valawai.mov.persistence.components.ComponentEntity;
 import eu.valawai.mov.persistence.logs.LogEntity;
 import io.quarkus.test.junit.QuarkusTest;
 import io.vertx.core.json.Json;
@@ -39,6 +49,12 @@ public class QueryComponentsManagerTest extends MovEventTestCase {
 	String queryComponentstQueueName;
 
 	/**
+	 * The name of the queue to send the query to obtain some components.
+	 */
+	@ConfigProperty(name = "mp.messaging.outgoing.components_page.queue.name", defaultValue = "valawai/component/page")
+	String componentsPagetQueueName;
+
+	/**
 	 * Check that cannot ask for components with an invalid payload.
 	 */
 	@Test
@@ -54,20 +70,89 @@ public class QueryComponentsManagerTest extends MovEventTestCase {
 	}
 
 	/**
-	 * Check that query some components.
+	 * Check that query return an empty page.
 	 */
 	@Test
-	public void shouldQuerySomeComponents() {
+	public void shouldQueryREturnrEmptyPage() {
+
+		final var queue = this.waitOpenQueue(this.componentsPagetQueueName);
 
 		final var payload = new QueryComponentsPayload();
+		payload.id = nextUUID().toString();
 		payload.offset = 3;
 		payload.limit = 7;
 		payload.pattern = nextUUID().toString();
 
 		this.executeAndWaitUntilNewLog(() -> this.assertPublish(this.queryComponentstQueueName, payload));
 
-		assertEquals(1l, LogEntity.count("level = ?1 and payload = ?2", LogLevel.INFO, Json.encodePrettily(payload))
+		assertEquals(1l, LogEntity.count("level = ?1 and message like ?2", LogLevel.INFO, ".+" + payload.id + ".*")
 				.await().atMost(Duration.ofSeconds(30)));
+		final var msg = queue.waitReceiveMessage(ComponentsPagePayload.class);
+		assertEquals(payload.id, msg.queryId);
+		assertEquals(0l, msg.total);
+		assertNull(msg.components);
+
+	}
+
+	/**
+	 * Check that query with pattern and type.
+	 */
+	@Test
+	public void shouldQueryWithPatternAndType() {
+
+		final var queue = this.waitOpenQueue(this.componentsPagetQueueName);
+
+		final var query = new QueryComponentsPayload();
+		query.id = nextUUID().toString();
+		query.offset = rnd().nextInt(2, 5);
+		query.limit = rnd().nextInt(5, 11);
+		final var pattern = ".*1.*";
+		query.pattern = "/" + pattern + "/";
+		query.order = "description,-name";
+		query.offset = rnd().nextInt(2, 5);
+		final var type = next(ComponentType.values());
+		query.type = type.name();
+
+		final var expected = new ComponentsPagePayload();
+		expected.queryId = query.id;
+
+		final var max = query.offset + query.limit + 10;
+		final var filter = Filters.and(
+				Filters.or(Filters.regex("name", pattern), Filters.regex("description", pattern)),
+				Filters.eq("type", type));
+		expected.total = ComponentEntities.nextComponentsUntil(filter, max);
+
+		final List<ComponentEntity> components = this.assertItemNotNull(
+				ComponentEntity.mongoCollection().find(filter, ComponentEntity.class).collect().asList());
+		components.sort((l1, l2) -> {
+
+			var cmp = l1.description.compareTo(l2.description);
+			if (cmp == 0) {
+
+				cmp = l2.name.compareTo(l1.name);
+				if (cmp == 0) {
+
+					cmp = l1.id.compareTo(l2.id);
+				}
+			}
+
+			return cmp;
+		});
+		expected.components = new ArrayList<>();
+		for (int i = query.offset; i < query.offset + query.limit && i < components.size(); i++) {
+
+			final var component = components.get(i);
+			final var expectedComponent = ComponentPayloadTest.from(component);
+			expected.components.add(expectedComponent);
+		}
+
+		this.executeAndWaitUntilNewLog(() -> this.assertPublish(this.queryComponentstQueueName, query));
+
+		assertEquals(1l, LogEntity.count("level = ?1 and message like ?2", LogLevel.INFO, ".+" + query.id + ".*")
+				.await().atMost(Duration.ofSeconds(30)));
+
+		final var page = queue.waitReceiveMessage(ComponentsPagePayload.class);
+		assertEquals(expected, page);
 
 	}
 
