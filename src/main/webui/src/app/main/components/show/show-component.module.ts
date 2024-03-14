0a9e162e4d6cbe7d0417ea08af58a3760da135b5
Bruno Rosell i Gui <rosell@iiia.csic.es>
2024-03-14 13:15:24 +0100

    Fix show component ui

--- src/main/webui/src/app/main/components/show/show-component.module.ts
+++ src/main/webui/src/app/main/components/show/show-component.module.ts
@@ -12,17 +12,11 @@ import { CommonModule } from '@angular/common';
 import { ShowComponentRoutingModule } from './show-component-routing.module';
 import { ShowComponentComponent } from './show-component.component';
 
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatMenuModule } from '@angular/material/menu';
 import { LoadingModule } from 'src/app/shared/loading/loading.module';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+
 
 @NgModule({
 	declarations: [
@@ -31,17 +25,10 @@ import { LoadingModule } from 'src/app/shared/loading/loading.module';
 	imports: [
 		CommonModule,
 		ShowComponentRoutingModule,
-		MatPaginatorModule,
-		MatTableModule,
-		MatFormFieldModule,
-		MatInputModule,
-		ReactiveFormsModule,
 		MatIconModule,
 		MatButtonModule,
-		MatSelectModule,
-		MatCheckboxModule,
-		MatMenuModule,
-		LoadingModule
+		LoadingModule,
+		NgxJsonViewerModule
 	],
 	exports: [
 	],
