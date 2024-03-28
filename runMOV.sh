23db825b8c5e2b245f38f781fd08db7178ad47bd
Bruno Rosell i Gui <rosell@iiia.csic.es>
2024-03-28 18:19:29 +0100

    Fix check if is necessary to make the container again

--- runMOV.sh
+++ runMOV.sh
@@ -5,8 +5,19 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd $DIR >/dev/null
 
-	if [ -z "$(docker images -q valawai/mov:latest> /dev/null)" ]; then
+	if [ -z "$(docker images -q valawai/mov:latest)" ]; then
+		# No image exist
 		./buildDockerImages.sh latest
+
+	else
+
+		SRC_DATE=$(TZ=UTC0 git log -1 --quiet --date=local --format="%cd" --date=format-local:'%Y-%m-%dT%H:%M:%S.000000000Z')
+		IMG_DATE=$(docker inspect -f '{{ .Created }}' valawai/mov:latest)
+		if [[ $SRC_DATE > $IMG_DATE ]]; then
+			# The image is older that the last modified file
+			./buildDockerImages.sh latest
+		fi
 	fi
+
 	docker-compose -f src/main/docker/docker-compose.yml up -d
 fi
