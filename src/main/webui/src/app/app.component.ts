f7d8b131b6180a9360ca11bdeb0e833d151d23a5
Bruno Rosell i Gui <rosell@iiia.csic.es>
2024-11-29 17:45:36 +0100

    Fix src app

--- src/main/webui/src/app/app.component.ts
+++ src/main/webui/src/app/app.component.ts
@@ -7,15 +7,16 @@
 */
 
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
+	standalone: true,
 	selector: 'app-root',
+	imports: [RouterOutlet],
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-
 
 	/**
 	 * Create the component.
