a74251fb0bbe35edffdfd32ea8e5741daa0af2f9
Bruno Rosell i Gui <rosell@iiia.csic.es>
2025-07-29 14:54:18 +0200

    Fix endpoint editor to use editor models

--- src/main/webui/src/app/main/topology/design/editor/endpoint-editor.component.ts
+++ src/main/webui/src/app/main/topology/design/editor/endpoint-editor.component.ts
@@ -8,29 +8,73 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { TopologyConnectionEndpoint } from '@app/shared/mov-api';
+import { ChannelSchema, } from '@app/shared/mov-api';
 import { Subscription } from 'rxjs';
-import { GraphModule } from '@app/shared/graph';
 import { EditorTopologyService } from './editor-topology.service';
+import { EditorNode } from './editor-node.model';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { EditorEndpoint } from './editor-endpoint.model';
+import { MatInputModule } from '@angular/material/input';
+import { toChannelName, toRegexp } from '@app/shared';
+import { GraphModule } from '@app/shared/graph';
 
 
+
+export function requiredNode(): ValidatorFn {
+
+	return (control: AbstractControl): ValidationErrors | null => {
+
+		if (control != null && (control.value == null || typeof control.value === 'string')) {
+
+			return { 'required': true };
+
+		} else {
+
+			return null;
+		}
+	};
+}
+
+export function requiredChannel(editor: EndpointEditorComponent): ValidatorFn {
+
+	return (control: AbstractControl): ValidationErrors | null => {
+
+		if (control != null &&
+			control.value === null ||
+			(typeof control.value === 'string' &&
+				editor.possibleChannels.findIndex(c => c.name === control.value) < 0)
+			|| (
+				typeof control.value === 'object' &&
+				editor.possibleChannels.findIndex(c => c.name === control.value.name) < 0
+			)
+		) {
+
+			return { 'required': true };
+
+
+		} else {
+			return null;
+		}
+
+	};
+}
 
 @Component({
 	standalone: true,
-	selector: 'app-topology-connection-endpoint-editor',
+	selector: 'app-topology-endpoint-editor',
 	imports: [
 		CommonModule,
 		ReactiveFormsModule,
-		MatSelectModule,
 		MatFormFieldModule,
+		MatInputModule,
+		MatAutocompleteModule,
 		GraphModule
 	],
 	templateUrl: './endpoint-editor.component.html'
 })
-export class TopologyConnectionEndpointEditorComponent implements OnInit, OnDestroy {
+export class EndpointEditorComponent implements OnInit, OnDestroy {
 
 	/**
 	 * This is {@code true} if the end point is a source.
@@ -47,37 +91,43 @@ export class TopologyConnectionEndpointEditorComponent implements OnInit, OnDest
 	 * Notify when the endpoint is valid.
 	 */
 	@Output()
-	public endpointUpdated = new EventEmitter<TopologyConnectionEndpoint>();
+	public endpointUpdated = new EventEmitter<EditorEndpoint>();
 
 	/**
 	 * The form to edit the endpoint.
 	 */
 	public endpointForm = new FormGroup(
 		{
-			nodeTag: new FormControl<string | null>(null, Validators.required),
-			channel: new FormControl<string | null>(null, Validators.required)
+			node: new FormControl<EditorNode | string | null>(null, {
+				validators: requiredNode(),
+				updateOn: 'change'
+			}),
+			channel: new FormControl<ChannelSchema | string | null>(null, {
+				validators: requiredChannel(this),
+				updateOn: 'change'
+			}),
 		}
 	);
 
 	/**
-	 * The subscription to the change to the node tag.
+	 * The filtered nodes.
 	 */
-	private nodeTagChangedSubscription: Subscription | null = null;
+	public possibleNodes: EditorNode[] = [];
 
 	/**
-	 * The subscription to the changes of the endpoint form.
+	 * The filtered nodes.
 	 */
-	public endpointStatusSubscription: Subscription | null = null;
+	public possibleChannels: ChannelSchema[] = [];
+
+	/**
+	 * The subscription to the change to the node tag.
+	 */
+	private subscruptions: Subscription[] = [];
 
 	/**
 	 * The the last valid endpoint.
 	 */
-	private lastValid: TopologyConnectionEndpoint | null = null;
-
-	/**
-	 * The name of the posible channels for the	selected node.
-	 */
-	public posibleChannels: string[] = [];
+	private lastValid: EditorEndpoint | null = null;
 
 
 	/**
@@ -85,31 +135,113 @@ export class TopologyConnectionEndpointEditorComponent implements OnInit, OnDest
 	 */
 	public ngOnInit() {
 
-		this.endpointStatusSubscription = this.endpointForm.statusChanges.subscribe(
-			{
-				next: status => {
+		this.updatePossibleNodes();
+		this.subscruptions.push(
 
-					if (status == 'VALID') {
+			this.endpointForm.controls.node.valueChanges.subscribe(
+				{
+					next:
+						value => {
 
-						var newEndpoint = new TopologyConnectionEndpoint();
-						newEndpoint.nodeTag = this.endpointForm.controls.nodeTag.value || null;
-						newEndpoint.channel = this.endpointForm.controls.channel.value || null;
-						if (JSON.stringify(this.lastValid) != JSON.stringify(newEndpoint)) {
+							this.updatePossibleNodes(value);
+							if (typeof value === 'object') {
 
-							this.lastValid = newEndpoint;
-							this.endpointUpdated.emit(newEndpoint);
+								this.endpointForm.controls.channel.enable();
+								this.updatePossibleChannels();
+								this.endpointForm.controls.channel.updateValueAndValidity();
+							}
+						}
+				}
+			)
+		);
+		this.subscruptions.push(
+
+			this.endpointForm.controls.channel.valueChanges.subscribe(
+				{
+					next: value => this.updatePossibleChannels(value)
+				}
+			)
+		);
+
+		this.subscruptions.push(
+			this.endpointForm.statusChanges.subscribe(
+				{
+					next: status => {
+
+						if (status == 'VALID') {
+
+							var value = this.endpointForm.value;
+							var node = value.node as EditorNode;
+							var channel = value.channel as ChannelSchema;
+							var newEndpoint = new EditorEndpoint(node.id, channel.name, this.isSource);
+							if (JSON.stringify(this.lastValid) != JSON.stringify(newEndpoint)) {
+
+								this.lastValid = newEndpoint;
+								this.endpointUpdated.emit(newEndpoint);
+							}
 						}
 					}
 				}
-			}
+			)
 		);
 
+	}
 
-		this.nodeTagChangedSubscription = this.endpointForm.controls.nodeTag.valueChanges.subscribe(
-			{
-				next: () => this.updatePossibleChannels()
+	/**
+	 * Called when whant to change the possible nodes.
+	 */
+	private updatePossibleNodes(value: EditorNode | string | null = null) {
+
+		this.possibleNodes = [];
+		var regexp = toRegexp(value);
+		for (var node of this.topology.nodes) {
+
+			if (regexp.test(node.name)) {
+
+				this.possibleNodes.push(node);
 			}
-		);
+		}
+
+		this.possibleNodes.sort((n1, n2) => n1.compareTo(n2));
+	}
+
+
+
+	/**
+	 * Called when whant to change the possible channels.
+	 */
+	private updatePossibleChannels(value: ChannelSchema | string | null = null) {
+
+		this.possibleChannels = [];
+		var node = this.endpointForm.controls.node.value;
+		if (node != null && typeof node === 'object') {
+
+			if (node.component! != null && node.component.channels != null) {
+
+				var pattern = toRegexp(value);
+				for (var channel of node.component.channels) {
+
+					if (channel.name != null && pattern.test(channel.name)
+						&& ((channel.publish != null && this.isSource)
+							|| (channel.subscribe != null && !this.isSource))
+					) {
+
+						this.possibleChannels.push(channel);
+					}
+				}
+
+				this.possibleChannels.sort((c1, c2) => c1.name!.localeCompare(c2.name!));
+			}
+			if (this.endpointForm.controls.channel.disabled) {
+
+				this.endpointForm.controls.channel.enable();
+			}
+
+
+		} else if (this.endpointForm.controls.channel.enabled) {
+
+			this.endpointForm.controls.channel.disable();
+		}
 	}
 
 	/**
@@ -117,17 +249,12 @@ export class TopologyConnectionEndpointEditorComponent implements OnInit, OnDest
 	 */
 	public ngOnDestroy() {
 
-		if (this.endpointStatusSubscription != null) {
+		for (var subscription of this.subscruptions) {
 
-			this.endpointStatusSubscription.unsubscribe();
-			this.endpointStatusSubscription = null;
+			subscription.unsubscribe();
 		}
 
-		if (this.nodeTagChangedSubscription != null) {
-
-			this.nodeTagChangedSubscription.unsubscribe();
-			this.nodeTagChangedSubscription = null;
-		}
+		this.subscruptions.splice(0, this.subscruptions.length);
 	}
 
 
@@ -136,53 +263,72 @@ export class TopologyConnectionEndpointEditorComponent implements OnInit, OnDest
 	 * Set the endpoint to edit.
 	 */
 	@Input()
-	public set endpoint(endpoint: TopologyConnectionEndpoint | null | undefined) {
+	public set endpoint(endpoint: EditorEndpoint | null | undefined) {
 
-		this.endpointForm.patchValue(
+		var channel: ChannelSchema | null = null;
+		var node = this.topology.getNodeWith(endpoint?.nodeId);
+		if (node != null && node.component != null && node.component.channels != null) {
+
+			channel = node.component.channels.find(c => c.name == endpoint?.channel) || null;
+		}
+		this.lastValid = endpoint || null;
+		this.endpointForm.setValue(
 			{
-				nodeTag: endpoint?.nodeTag || null,
-				channel: endpoint?.channel || null
+				node: node,
+				channel: channel
 			},
 			{
 				emitEvent: false
 			}
 		);
-		this.updatePossibleChannels();
+		this.updatePossibleNodes(node);
+		this.updatePossibleChannels(channel);
+	}
+
+	/**
+	 * Return the text to display for a node. 
+	 */
+	public displayNode(node: EditorNode | string | null): string {
+
+		if (node == null || typeof node === 'string') {
+
+			return '';
+
+		} else if (node.component != null && node.component.name != null) {
+
+			return node.component.name;
+
+		} else if (node.fisrtEndPointWithIsSource != null) {
+
+			return 'NOTIFICATION TO DO';
+
+		} else {
+
+			return node.id;
+		}
 
 	}
 
 	/**
-	 * Define the posible channels that can be selected.
+	 * Return the text to display for a channel. 
 	 */
-	private updatePossibleChannels() {
+	public displayChannel(channel: ChannelSchema | string | null): string {
 
+		if (channel == null || typeof channel === 'string') {
 
-		this.posibleChannels = [];
-		/*
-		if (this.topology != null) {
+			return '';
 
-			var node = this.topology.nodes.find(n => n.id == this.endpointForm.controls.nodeTag.value);
-			if (node && 'component' in node.model && node.model.component && node.model.component.channels) {
+		} else {
 
-				for (var channel of node.model.component.channels) {
-
-					if (channel.name && ((this.isSource && channel.publish != null) || (!this.isSource && channel.subscribe != null))) {
-
-						this.posibleChannels.push(channel.name);
-					}
-				}
-
-				this.posibleChannels.sort();
+			var name = toChannelName(channel.name);
+			if (name == null) {
+				name = '';
 			}
-
-		}
-		*/
-
-		if (this.endpointForm.controls.channel.value != null && this.posibleChannels.indexOf(this.endpointForm.controls.channel.value) < 0) {
-
-			this.endpointForm.controls.channel.setErrors({ 'undefined': true });
+			return name;
 		}
 
 	}
+
+
 
 }
