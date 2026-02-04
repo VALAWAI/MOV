e6726469244121ba61cd4df201b5588fd39e7116
Bruno Rosell i Gui <rosell@iiia.csic.es>
2026-02-04 11:07:27 +0100

    Fix unit test to get the live connection

--- src/main/java/eu/valawai/mov/persistence/live/connections/GetLiveConnection.java
+++ src/main/java/eu/valawai/mov/persistence/live/connections/GetLiveConnection.java
@@ -16,7 +16,7 @@ import org.bson.types.ObjectId;
 
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.UnwindOptions;
 
 import eu.valawai.mov.api.v2.design.topologies.Topology;
 import eu.valawai.mov.api.v2.live.connections.LiveConnection;
@@ -29,6 +29,7 @@ import io.smallrye.mutiny.Uni;
  * Obtain a {@link LiveConnection} from the database.
  *
  * @see LiveConnection
+ * @see TopologyConnectionEntity
  *
  * @author VALAWAI
  */
@@ -78,17 +79,111 @@ public class GetLiveConnection extends AbstractEntityOperator<LiveConnection, Ge
 
 		final var pipeline = new ArrayList<Bson>();
 		pipeline.add(Aggregates.match(Filters.eq("_id", this.id)));
+		pipeline.add(Aggregates.unwind("$notifications", new UnwindOptions().preserveNullAndEmptyArrays(true)));
+		pipeline.add(Aggregates.lookup(ComponentEntity.COLLECTION_NAME, "notifications.node.componentId", "_id",
+				"notificationComponent"));
+		pipeline.add(Document.parse("""
+				{
+				    "$group": {
+				      "_id": "$_id",
+				      "enabled": { "$first": "$enabled" },
+				      "createTimestamp": {
+				        "$first": "$createTimestamp"
+				      },
+				      "updateTimestamp": {
+				        "$first": "$updateTimestamp"
+				      },
+				      "convertCode": {
+				        "$first": "$targetMessageConverterJSCode"
+				      },
+				      "source": { "$first": "$source" },
+				      "target": { "$first": "$target" },
+				      "notifications": {
+				        "$push": {
+				          "$cond": {
+				            "if": {
+				                "$ne":[
+				                {
+				                	"$size":
+				                	{
+				                  		"$ifNull":
+				                  		[
+				                    		"$notificationComponent",
+				                    		[]
+				                  		]
+				                	}
+				                },
+				                0
+				              ]
+				            },
+				            "then": {
+				              "node": {
+				                "component": {
+				                  "_id": { "$first":"$notificationComponent._id"},
+				                  "name": { "$first":"$notificationComponent.name"},
+				                  "description": { "$first":"$notificationComponent.description"},
+				                  "type": { "$first":"$notificationComponent.type"}
+				                },
+				                "channelName": "$notifications.node.channelName"
+				              },
+				              "enabled": "$notifications.enabled",
+				              "convertCode": "$notifications.notificationMessageConverterJSCode"
+				            },
+				            "else": "$$REMOVE"
+				          }
+				        }
+				      }
+				    }
+				}
+				"""));
 
 		pipeline.add(
 				Aggregates.lookup(ComponentEntity.COLLECTION_NAME, "source.componentId", "_id", "sourceComponent"));
 		pipeline.add(
 				Aggregates.lookup(ComponentEntity.COLLECTION_NAME, "target.componentId", "_id", "targetComponent"));
 
-		pipeline.add(Aggregates.project(Projections.fields(
-				Projections.include("id", "enabled", "createTimestamp", "updateTimestamp", "source", "target",
-						"notifications"),
-				Projections.computed("source.component", new Document("$first", "$sourceComponent")),
-				Projections.computed("target.component", new Document("$first", "$targetComponent")))));
+		pipeline.add(Document.parse("""
+				{
+				    "$project": {
+				      "_id": 1,
+				      "enabled": 1,
+				      "createTimestamp": 1,
+				      "updateTimestamp": 1,
+				      "notifications": 1,
+				      "convertCode": 1,
+				      "source": {
+				        "component": {
+				          "_id": { "$first": "$sourceComponent._id" },
+				          "name": {
+				            "$first": "$sourceComponent.name"
+				          },
+				          "description": {
+				            "$first": "$sourceComponent.description"
+				          },
+				          "type": {
+				            "$first": "$sourceComponent.type"
+				          }
+				        },
+				        "channelName": "$source.channelName"
+				      },
+				      "target": {
+				        "component": {
+				          "_id": { "$first": "$targetComponent._id" },
+				          "name": {
+				            "$first": "$targetComponent.name"
+				          },
+				          "description": {
+				            "$first": "$targetComponent.description"
+				          },
+				          "type": {
+				            "$first": "$targetComponent.type"
+				          }
+				        },
+				        "channelName": "$target.channelName"
+				      }
+				    }
+				  }
+				  """));
 
 		return TopologyConnectionEntity.mongoCollection().aggregate(pipeline, LiveConnection.class).collect().first();
 
